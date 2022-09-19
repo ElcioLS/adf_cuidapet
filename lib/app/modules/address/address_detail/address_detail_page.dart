442424a6240b5baa8b0b4091af8de3f1cb5e0cf1
ElcioLS <elciols@gmail.com>
2022-09-19 19:51:36 -0300

    Cadastrando endereço no sqlite

--- lib/app/modules/address/address_detail/address_detail_page.dart
+++ lib/app/modules/address/address_detail/address_detail_page.dart
@@ -2,8 +2,11 @@ import 'package:adf_cuidapet/app/core/ui/extensions/size_screen_extension.dart';
 import 'package:adf_cuidapet/app/core/ui/extensions/theme_extension.dart';
 import 'package:adf_cuidapet/app/core/ui/widgets/cuidapet_default_button.dart';
 import 'package:adf_cuidapet/app/models/place_model.dart';
+import 'package:adf_cuidapet/app/modules/address/address_detail/address_detail_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:mobx/mobx.dart';
 
 class AddressDetailPage extends StatefulWidget {
   final PlaceModel place;
@@ -15,6 +18,28 @@ class AddressDetailPage extends StatefulWidget {
 }
 
 class _AddressDetailPageState extends State<AddressDetailPage> {
+  final _additionalEC = TextEditingController();
+  final controller = Modular.get<AddressDetailController>();
+  late final ReactionDisposer addressEntityDisposer;
+
+  @override
+  void initState() {
+    super.initState();
+    addressEntityDisposer =
+        reaction((_) => controller.addressEntity, (addressEntity) {
+      if (addressEntity != null) {
+        Navigator.pop(context, addressEntity);
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _additionalEC.dispose();
+    addressEntityDisposer();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -71,6 +96,7 @@ class _AddressDetailPageState extends State<AddressDetailPage> {
           Padding(
             padding: const EdgeInsets.all(8.0),
             child: TextFormField(
+              controller: _additionalEC,
               decoration: const InputDecoration(
                 labelText: 'Complemento',
               ),
@@ -81,7 +107,9 @@ class _AddressDetailPageState extends State<AddressDetailPage> {
             height: 60.h,
             child: CuidapetDefaultButton(
               label: 'Salvar',
-              onPressed: () {},
+              onPressed: () {
+                controller.saveAddress(widget.place, _additionalEC.text);
+              },
             ),
           ),
           const SizedBox(height: 20),
