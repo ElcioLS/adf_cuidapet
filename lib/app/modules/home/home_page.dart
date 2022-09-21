1967a690a46fee4975a7b4c736871547433c50b1
ElcioLS <elciols@gmail.com>
2022-09-20 22:44:38 -0300

    Design - Criando componente de categorias

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:adf_cuidapet/app/core/life_cycle/page_life_cycle_state.dart';
+import 'package:adf_cuidapet/app/core/ui/extensions/theme_extension.dart';
 import 'package:adf_cuidapet/app/entities/address_entity.dart';
 import 'package:adf_cuidapet/app/modules/home/home_controller.dart';
 import 'package:adf_cuidapet/app/modules/home/widgets/home_appbar.dart';
@@ -6,6 +7,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 
 part 'widgets/home_address_widget.dart';
+part 'widgets/home_categories_widget.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -31,6 +33,9 @@ class _HomePageState extends PageLifeCycleState<HomeController, HomePage> {
                 controller: controller,
               ),
             ),
+            const SliverToBoxAdapter(
+              child: _HomeCategoriesWidget(),
+            )
           ];
         },
         body: Container(),
