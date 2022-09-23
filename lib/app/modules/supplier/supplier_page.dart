9e6eb1f91fca6aa01c9e6069fc85c5eb912318d5
ElcioLS <elciols@gmail.com>
2022-09-23 20:16:26 -0300

    Criando layout da tela de detalhe do fornecedor

--- lib/app/modules/supplier/supplier_page.dart
+++ lib/app/modules/supplier/supplier_page.dart
@@ -1,3 +1,6 @@
+import 'package:adf_cuidapet/app/core/ui/extensions/theme_extension.dart';
+import 'package:adf_cuidapet/app/modules/supplier/widgets/supplier_detail.dart';
+import 'package:adf_cuidapet/app/modules/supplier/widgets/supplier_service_widget.dart';
 import 'package:flutter/material.dart';
 
 class SupplierPage extends StatelessWidget {
@@ -6,10 +9,53 @@ class SupplierPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Supplier'),
+      floatingActionButton: FloatingActionButton.extended(
+        label: const Text('Realizar Agendamento.'),
+        onPressed: () {},
+        icon: const Icon(Icons.schedule),
+        backgroundColor: context.primaryColor,
       ),
-      body: Container(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            expandedHeight: 200,
+            pinned: true,
+            flexibleSpace: FlexibleSpaceBar(
+              stretchModes: const [
+                StretchMode.zoomBackground,
+                StretchMode.fadeTitle,
+              ],
+              background: Image.network(
+                'https://super.abril.com.br/wp-content/uploads/2018/05/filhotes-de-cachorro-alcanc3a7am-o-c3a1pice-de-fofura-com-8-semanas1.png?quality=90&strip=info&resize=850,567',
+                fit: BoxFit.cover,
+                errorBuilder: (context, error, stackTrace) =>
+                    const SizedBox.shrink(),
+              ),
+            ),
+          ),
+          const SliverToBoxAdapter(
+            child: SupplierDetail(),
+          ),
+          const SliverToBoxAdapter(
+            child: Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Text(
+                'Serviços (0 selecionados)',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
+              ),
+            ),
+          ),
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              childCount: 80,
+              (context, index) {
+                return const SupplierServiceWidget();
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
